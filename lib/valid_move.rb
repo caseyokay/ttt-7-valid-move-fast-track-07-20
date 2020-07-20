a0231ce496aac87d384f7bebff8ca1e1d35b395a
Casey Bivens <caseyoksana@gmail.com>
2020-07-20 20:20:17 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -24,3 +24,16 @@
 #   end
 # end
 
+def valid_move?(board, index)
+  if index.between?(1,9)
+    if !position_taken?(board, index)
+      true
+    end
+  end
+
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
