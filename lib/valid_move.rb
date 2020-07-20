31fb8c26addcbaf26009ca0515288ea8fa9aad58
Casey Bivens <caseyoksana@gmail.com>
2020-07-20 20:20:32 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,39 @@
-# code your #valid_move? method here
+# # code your #valid_move? method here
+# #between?(min, max) → true or falseclick to toggle source
+# #Returns false if obj <=> min is less than zero or if obj <=> max is greater than zero, true otherwise.
+# #!singlebang negates the value it is placed in front of, returns a boleen value  
+
+# def valid_move (board, index)
+#   if index.between?(1,9)
+#     if !position_taken?
+#       true
+#     end
+#   end
+      
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+# # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+# def position_taken? (board, index)
+#   taken = nil
+#   if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+#     taken = false
+#   else
+#     taken = true
+#   end
+# end
+
+def valid_move?(board, index)
+  if index.between?(1,9)
+    if !position_taken?(board, index)
+      true
+    end
+  end
+
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
